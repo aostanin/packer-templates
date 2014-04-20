a085e55d1b5f636f97ba0c13ecd2312d50803ecd
Andrei Ostanin <aostanin@gmail.com>
2014-04-20 14:08:27 +0900

    set systemd settings during install

--- gentoo/scripts/settings.sh
+++ gentoo/scripts/settings.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 
 chroot /mnt/gentoo /bin/bash <<EOF
-#/usr/lib/systemd/systemd-localed &
-#/usr/lib/systemd/systemd-hostnamed &
+/usr/bin/dbus-daemon --system
+/usr/lib/systemd/systemd-localed &
+/usr/lib/systemd/systemd-timedated &
+/usr/lib/systemd/systemd-hostnamed &
 
 echo "${GENTOO_LOCALE} ${GENTOO_LOCALE/*./}" > /etc/locale.gen
 locale-gen
 eselect locale set ${GENTOO_LOCALE}
-#localectl set-locale LANG=${GENTOO_LOCALE}
 
-#hostnamectl set-hostname ${GENTOO_HOSTNAME}
-
-#localectl set-keymap ${GENTOO_KEYMAP}
+localectl set-locale LANG=${GENTOO_LOCALE}
+hostnamectl set-hostname ${GENTOO_HOSTNAME}
+localectl set-keymap ${GENTOO_KEYMAP}
 EOF
 
